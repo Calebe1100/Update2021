73f352419a53f871c049289693128c8f96803f30
Calebe Liberio <calebe.pedroso@paiva.com.br>
2021-10-21 08:41:16 -0300

    Criando primeiro crud básico

---
+++ LiberWebMVC/Data/LiberWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LiberWebMVC.Models;
+
+namespace LiberWebMVC.Data
+{
+    public class LiberWebMVCContext : DbContext
+    {
+        public LiberWebMVCContext (DbContextOptions<LiberWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LiberWebMVC.Models.Department> Department { get; set; }
+    }
+}
--- LiberWebMVC/Startup.cs
+++ LiberWebMVC/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LiberWebMVC.Data;
 
 namespace LiberWebMVC
 {
@@ -24,6 +26,9 @@ namespace LiberWebMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<LiberWebMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LiberWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
